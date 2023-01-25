c508ad0b90f5414791604277e6971bb348389883
Angel Olmos <anolgi@gmail.com>
2023-01-25 12:54:03 +0100

    Introduint asserts

--- src/test/java/org/cefire/library/BookCollectionTest.java
+++ src/test/java/org/cefire/library/BookCollectionTest.java
@@ -1,11 +1,15 @@
 package org.cefire.library;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class BookCollectionTest {
 
     @Test
     public void ShouldFindBookByISBN(){
+        final String isbnToLocate = "un-isbn-2";
         BookCollection books = new BookCollection(new Book[]{
                 new Book("un-isbn-1","un título 1","un autor 1"),
                 new Book("un-isbn-2","un título 2","un autor 2"),
@@ -13,20 +17,9 @@ public class BookCollectionTest {
                 new Book("un-isbn-3","un título 3","un autor 3")
         });
 
-        if(!books.find("un-isbn-2").isEmpty()){
-            boolean allCoincidencesAreCorrect = true;
-            for (Book book : books.find("un-isbn-2")) {
-                if(!book.getISBN().equals("un-isbn-666")){
-                    allCoincidencesAreCorrect = false;
-                    break;
-                }
-            }
+        List<Book> foundBooks = books.find(isbnToLocate);
+        Assertions.assertFalse(foundBooks::isEmpty);
 
-            if(allCoincidencesAreCorrect){
-                System.out.println("CORRECT ✓ --> Should find a Book by ISBN");
-                return;
-            }
-            System.out.println("ERROR ⛌ --> Should find a Book by ISBN");
-        }
+        foundBooks.forEach((Book book) -> Assertions.assertEquals(isbnToLocate,book.getISBN()));
     }
 }
