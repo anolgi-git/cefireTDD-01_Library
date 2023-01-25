1980e49e779d5f3893c2ef56f9f30521489e834a
Angel Olmos <anolgi@gmail.com>
2023-01-25 13:10:37 +0100

    Assert para emptyList

--- src/test/java/org/cefire/library/BookCollectionTest.java
+++ src/test/java/org/cefire/library/BookCollectionTest.java
@@ -12,8 +12,8 @@ public class BookCollectionTest {
         final String isbnToLocate = "un-isbn-2";
         BookCollection books = new BookCollection(new Book[]{
                 new Book("un-isbn-1","un título 1","un autor 1"),
-                new Book("un-isbn-2","un título 2","un autor 2"),
-                new Book("un-isbn-2","un título 2","un autor 2"),
+                new Book(isbnToLocate,"un título 2","un autor 2"),
+                new Book(isbnToLocate,"un título 2","un autor 2"),
                 new Book("un-isbn-3","un título 3","un autor 3")
         });
 
@@ -21,5 +21,17 @@ public class BookCollectionTest {
         Assertions.assertFalse(foundBooks::isEmpty);
 
         foundBooks.forEach((Book book) -> Assertions.assertEquals(isbnToLocate,book.getISBN()));
+    }
+
+    @Test
+    public void shouldGetAnEmptyListIfNoMatchesAreFound(){
+        final String isbnToLocate = "un-isbn-no-existe";
+        BookCollection books = new BookCollection(new Book[]{
+                new Book("un-isbn-1","un título 1","un autor 1"),
+                new Book("un-isbn-2","un título 2","un autor 2"),
+                new Book("un-isbn-3","un título 3","un autor 3")
+        });
+
+        Assertions.assertTrue(books.find(isbnToLocate).isEmpty());
     }
 }
